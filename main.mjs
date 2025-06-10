c878e27656c2fb1e0a7d9525d8e499123467f805
fengcenio <github@fengcen.io>
2025-06-11 07:29:25 +0800

    feat: ✨ add example shortcut with manifest and main module

---
+++ main.mjs
@@ -0,0 +1,12 @@
+export default {
+  shortcuts: [
+    {
+      id: 'ShortcutExample',
+      displayName: 'Shortcut Example',
+      handle: (parameters) => {
+        sliprail.showAlert('Shortcut Example');
+      },
+    },
+  ],
+  settings: [],
+};
